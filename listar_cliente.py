15ed71c2f5e5aa5365f13a50baa9969ba1a382b7
Guilherme <guilhermefreschix@gmail.com>
2024-10-02 14:17:51 -0300

    Melhoria em listar_cliente.py

--- listar_cliente.py
+++ listar_cliente.py
@@ -8,21 +8,20 @@ def clear_terminal():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 def listar_cliente():
-    while True:
-        msg_manner("Listar Clientes!")
-        clientes = []
+    clear_terminal()
+    msg_manner("Listar Clientes!")
+    clientes = []
 
-        with open("clientes.csv", "r", encoding="utf-8") as file:
-            reader = csv.DictReader(file)
-            for row in reader:
-                clientes.append({"Nome": row["Nome"], "Idade": row["Idade"], "Endereço": row["Endereço"], "Telefone": row["Telefone"]})
+    with open("clientes.csv", "r", encoding="utf-8") as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            clientes.append({"Nome": row["Nome"], "Idade": row["Idade"], "Endereço": row["Endereço"], "Telefone": row["Telefone"]})
 
-        # Verifica se há clientes
-        if not clientes:
-            print("Nenhum cliente encontrado.")
-        else:
-            for cliente in sorted(clientes, key=lambda cliente: cliente["Nome"]):
-                print(f"{cliente['Nome']}: {cliente['Idade']} anos. {cliente['Endereço']}. Telefone:{cliente['Telefone']}.")
+    # Verifica se há clientes
+    if not clientes:
+        print("Nenhum cliente encontrado.")
+    else:
+        for cliente in sorted(clientes, key=lambda cliente: cliente["Nome"]):
+            print(f"{cliente['Nome']}: {cliente['Idade']} anos. {cliente['Endereço']}. Telefone:{cliente['Telefone']}.")
 
-        input("Dê um Enter para sair...")
-        break
+    input("Dê um Enter para sair...")
